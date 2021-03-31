2e34153224551865e8975518e8c6d2f0c0f86dcb
anseal <bottomless.box@gmail.com>
2021-03-31 20:33:27 +0800

    added button to rerun test

--- src/pt.js
+++ src/pt.js
@@ -1,65 +1,68 @@
-import { normalize, schema } from './index.js';
+import { normalize, schema } from './index.js'
 
 const randomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 const repeat = (cnt, fn) => {
 	const results = []
-	for(let i = 0 ; i !== cnt ; ++i ) {
+	for (let i = 0; i !== cnt; ++i) {
 		results[i] = fn(i)
 	}
 	return results
 }
 
 // Define a users schema
-const user = new schema.Entity('users');
+const user = new schema.Entity('users')
 
+const content = new schema.Entity('content')
 // Define your comments schema
 const comment = new schema.Entity('comments', {
-  commenter: user
-});
+	commenter: user,
+	comment: { content },
+})
 
 // Define your article
 const article = new schema.Entity('articles', {
-  author: user,
-  comments: [comment]
-});
+	author: user,
+	comments: [comment],
+})
 
 const comments_cnt = 1000000
 const users_cnt = 100
 const users = repeat(users_cnt, (id) => {
-  return {
-    id,
-    name: "Paul",
-	surname: "Qu",
-	age: 10,
-	status: true,
-  }
+	return {
+		id,
+		name: 'Paul',
+		surname: 'Qu',
+		age: 10,
+		status: true,
+	}
 })
-const author = users[randomInRange(0,users_cnt-1)]
+const author = users[randomInRange(0, users_cnt - 1)]
 const originalData = {
-  "id": 123,
-  "author": author,
-  "title": "My awesome blog post",
-  "comments": repeat(comments_cnt, (id) => {
-    const user = users[randomInRange(0,users_cnt-1)]
-    return {
-      "id": id,
-      // "comment":
-      "commenter": {
-        "id": randomInRange(0,users_cnt-1),
-        "name": user.name
-      }
-    }
-  })
+	'id': 123,
+	'author': author,
+	'title': 'My awesome blog post',
+	'comments': repeat(comments_cnt, (id) => {
+		const user = users[randomInRange(0, users_cnt - 1)]
+		return {
+			'id': id,
+			'commenter': {
+				'id': randomInRange(0, users_cnt - 1),
+				'name': user.name,
+			},
+			comment: { content: randomInRange(0,1) ? null : {} },
+		}
+	}),
 }
 
 const test = () => {
 	console.log('!!!!!!!!!!!!!!')
 	const start = performance.now()
-	const normalizedData = normalize(originalData, article);
+	const normalizedData = normalize(originalData, article)
 	const duration = performance.now() - start
 	console.log(
-		duration,
+		duration
 		// JSON.stringify(normalizedData,undefined,'\t')
 	)
 }
-test()
+document.getElementById('run').addEventListener('click', test)
+//*/
